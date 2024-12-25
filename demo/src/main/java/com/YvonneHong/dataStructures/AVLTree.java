859319700a1a25ad9bc6f0439a9ba47d61e97aef
Yvonne Hong <yvonnehong2003@gmail.com>
2024-12-25 17:02:02 -0500

    update avl tree

--- demo/src/main/java/com/YvonneHong/dataStructures/AVLTree.java
+++ demo/src/main/java/com/YvonneHong/dataStructures/AVLTree.java
@@ -2,5 +2,12 @@ package com.YvonneHong.dataStructures;
 
 public class AVLTree {
     //check if the new push worked ? 
+    //maybe i should add a binary search tree (bst) ? 
+
+    //The AVL tree is a self-balancing binary search tree (BST), which means that 
+    //the nodes must follow the binary search tree property, where for any given node:
+    
+    //1) All nodes in the left subtree of a node are less than the node.
+    //2) All nodes in the right subtree of a node are greater than the node.
     
 }
