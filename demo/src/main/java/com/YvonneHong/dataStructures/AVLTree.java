d8980375cdd6cb8a02cbdbe7dd705bc341fb094b
Yvonne Hong <yvonnehong2003@gmail.com>
2024-12-26 18:00:57 -0500

    avl tree update

--- demo/src/main/java/com/YvonneHong/dataStructures/AVLTree.java
+++ demo/src/main/java/com/YvonneHong/dataStructures/AVLTree.java
@@ -6,8 +6,31 @@ public class AVLTree {
 
     //The AVL tree is a self-balancing binary search tree (BST), which means that 
     //the nodes must follow the binary search tree property, where for any given node:
-    
+
     //1) All nodes in the left subtree of a node are less than the node.
     //2) All nodes in the right subtree of a node are greater than the node.
+
+    //methods/operations to add: 
+    // 1) insertion
+    // 2) deletion 
+    // 3) lookup (searching)
+    // 4) find max value 
+    // 5) find min value 
+    // 6) inorder traversal (print out all the nodes)
+    // 7) preorder traversal (print out all the nodes)
+    // 8) return height of tree / height calculation
+    // 9) balance factor calculation
+    // 10) delete min value 
+    // 11) delete max value
+    // 12) clone the tree 
+    // 13) find successor of a node: The successor of a node is the node with the smallest key greater than the given node's key.
+    // 14) find predecessor: The predecessor of a node is the node with the largest key smaller than the given node's key.
+
+    //Helper methods: 
+    //1) right rotation (single rotation)
+    //2) left rotation (single rotation)
+    //3) left-right rotation (double rotation)
+    //4)right-left rotation (double rotation)
+    //misc: left rotate, right rotate (necessary for node insertion)
     
 }
