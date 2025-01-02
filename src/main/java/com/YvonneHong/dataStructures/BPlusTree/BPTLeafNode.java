896244f21a685d6e2cd98746ab81cf8d70a733b8
Yvonne Hong <yvonnehong2003@gmail.com>
2025-01-02 02:47:53 -0500

    one more done

--- src/main/java/com/YvonneHong/dataStructures/BPlusTree/BPTLeafNode.java
+++ src/main/java/com/YvonneHong/dataStructures/BPlusTree/BPTLeafNode.java
@@ -1,10 +1,88 @@
 package com.YvonneHong.dataStructures.BPlusTree;
-
 import java.util.ArrayList;
-import java.util.List;
+import java.util.List; 
 
-   // LeafNode class (stores data)
-   public class BPTLeafNode extends BPlusTreeNode {
+// LeafNode class (stores data)
+public class BPTLeafNode extends BPlusTreeNode {
+    private List<String> values; 
+    private BPTLeafNode next; 
+    private BPTLeafNode prev; 
+
+    public BPTLeafNode(int degree) {
+        super(degree); 
+        values = new ArrayList<>(); 
+        next = null; 
+        prev = null; 
+    }
+
+    //split the leaf node when it is full 
+    public BPTLeafNode split() {
+        int midIndex = keys.size() / 2;
+        BPTLeafNode newLeaf = new BPTLeafNode(degree); 
+
+        //move half of the keys and values to the new leaf node 
+        for(int i = midIndex; i < keys.size(); i++) {
+            newLeaf.addKey(keys.get(i)); 
+            newLeaf.addValue(values.get(i)); 
+        }
+
+        //remove the moved keys and values from the current leaf 
+        for(int i = midIndex; i < keys.size(); i++) {
+            keys.remove(i); 
+            values.remove(i); 
+        }
+
+        //link the old and new leaf nodes 
+        newLeaf.prev = this; 
+        newLeaf.next = this.next; 
+        if(this.next != null) {
+            this.next.prev = newLeaf; //??
+        }
+        this.next = newLeaf; 
+
+        return newLeaf; 
+    }
+
+    //add a key-value pair to this leaf node 
+    public void addKey(int key){
+        keys.add(key); 
+    }
+
+    //add a value to this leaf node 
+    public void addValue(String value) {
+        values.add(value); 
+    }
+
+    //insert a key-value pair into this leaf node 
+    @Override 
+    public void insert(int key, String value){
+        int index = 0; 
+        while((index < keys.size()) && (keys.get(index) < key)){
+            index++; 
+        }
+        keys.add(index, key);
+        values.add(index, value); 
+    }
+
+    //search for a value by key in this leaf node 
+    @Override
+    public String search(int key) {
+        int index = keys.indexOf(key); 
+        if(index >= 0) {
+            return values.get(index); 
+        }
+        return null; 
+    }
+
+    //print the leaf node for debugging 
+    @Override 
+    public void printNode() { //change name to printNodeKeys instead ? 
+        System.out.print("Leaf Node Keys: ");
+        for(int key : keys) {
+            System.out.print(key + " "); 
+        }
+        System.out.println();
+    }
     
 
   
--- src/main/java/com/YvonneHong/dataStructures/BPlusTree/BPTinternalNode.java
+++ src/main/java/com/YvonneHong/dataStructures/BPlusTree/BPTinternalNode.java
@@ -1,79 +1,132 @@
 package com.YvonneHong.dataStructures.BPlusTree;
+
 import java.util.ArrayList;
 import java.util.List;
 
-// InternalNode class (non-leaf node)
 public class BPTinternalNode extends BPlusTreeNode {
-    private List<BPlusTreeNode> children; 
+    private List<BPlusTreeNode> children;  // Child nodes (either internal or leaf)
 
     public BPTinternalNode(int degree) {
-        super(degree); //calls constructor in parent class 
+        super(degree);
         children = new ArrayList<>();
     }
 
-    //split the child node at the given index
-    public void splitChild(int index) {
-        BPlusTreeNode child = children.get(index); 
-        BPlusTreeNode newChild; 
-
-        if(child instanceof BPTinternalNode) {
-            newChild = ((BPTinternalNode) child).split(); 
-        } else {
-            newChild = ((BPTLeafNode) child).split(); 
-        }
-
-        keys.add(index, newChild.keys.get(0));
-        children.add(index + 1, newChild);
-    }
-
-    //insert a key into this internal node
+    // Override the insert method for internal nodes
     @Override
     public void insert(int key, String value) {
-        //find the child to insert into 
-        int index = findChildIndex(key); 
-        BPlusTreeNode child = children.get(index); 
-
-        //if the child node is full, split it first 
-        if(child.isFull()) {
-            splitChild(index); 
-            if(key > keys.get(index)) {
-                index++; 
+        // Logic to insert key into the appropriate child
+        int index = findChildIndex(key);
+        BPlusTreeNode child = children.get(index);
+        
+        if (child.isFull()) {
+            splitChild(index);
+            if (key > keys.get(index)) {
+                index++;
             }
         }
-
-        //insert the key in the child node 
-        child.insert(key, value); 
-
+        
+        child.insert(key, value);
     }
 
-    //search for a key in this internal node 
+    // Override the search method for internal nodes
     @Override
     public String search(int key) {
-        int index = findChildIndex(key); 
+        int index = findChildIndex(key);
         return children.get(index).search(key);
     }
 
-    //find the correct child index for the key
+    // Override the delete method for internal nodes
+    @Override
+    public void delete(int key) {
+        int index = findChildIndex(key);
+        children.get(index).delete(key);
+    }
+
+    // Override the print method for internal nodes
+    @Override
+    public void printNode() {
+        System.out.print("Internal Node Keys: ");
+        for (int key : keys) {
+            System.out.print(key + " ");
+        }
+        System.out.println();
+        for (BPlusTreeNode child : children) {
+            child.printNode();
+        }
+    }
+
+    // Helper method to find the index of the appropriate child
     private int findChildIndex(int key) {
-        int index = 0; 
-        while((index < keys.size()) && (key >= keys.get(index))) {
-            index++; 
+        int index = 0;
+        while (index < keys.size() && key >= keys.get(index)) {
+            index++;
         }
         return index;
     }
 
-    //print the internal node for debugging
-    @Override
-    public void printNode(){
-        System.out.print("Internal (non-leaf) Node Keys: "); 
-        for(int key: keys){
-            System.out.print(key + " "); 
+    // Split the child at a given index (this method can be extended)
+    public void splitChild(int index) {
+        // Get the child to split
+        BPlusTreeNode child = children.get(index);
+        
+        // Ensure the child is an internal node
+        if (child instanceof BPTinternalNode) {
+            BPTinternalNode internalChild = (BPTinternalNode) child;
+            internalChild.split();
+        } else {
+            BPTLeafNode leafChild = (BPTLeafNode) child;
+            leafChild.split();
         }
-        System.out.println(); 
-        for(BPlusTreeNode child: children) {
-            child.printNode(); 
+        
+        // Move the middle key of the child node up into the parent node
+        int middleKey = child.keys.get(child.keys.size() / 2);
+        keys.add(index, middleKey);
+        
+        // Add the new child node (the one split off) to the children list
+        BPlusTreeNode newChild;
+        if (child instanceof BPTinternalNode) {
+            newChild = ((BPTinternalNode) child).split();
+        } else {
+            newChild = ((BPTLeafNode) child).split();
         }
+
+        children.add(index + 1, newChild);
     }
 
-      
+    // Split the internal node (called by splitChild)
+    public BPTinternalNode split() {
+        // Determine the midpoint
+        int midIndex = keys.size() / 2;
+        BPTinternalNode newInternalNode = new BPTinternalNode(degree);
+
+        // Move half of the keys and children to the new internal node
+        for (int i = midIndex + 1; i < keys.size(); i++) {
+            newInternalNode.addKey(keys.get(i));
+        }
+
+        for (int i = midIndex + 1; i < children.size(); i++) {
+            newInternalNode.addChild(children.get(i));
+        }
+
+        // Remove the moved keys and children from the current internal node
+        for (int i = midIndex; i < keys.size(); i++) {
+            keys.remove(i);
+        }
+
+        for (int i = midIndex + 1; i < children.size(); i++) {
+            children.remove(i);
+        }
+
+        return newInternalNode;
+    }
+
+    // Helper method to add key
+    public void addKey(int key) {
+        keys.add(key);
+    }
+
+    // Helper method to add child node
+    public void addChild(BPlusTreeNode child) {
+        children.add(child);
+    }
 }
